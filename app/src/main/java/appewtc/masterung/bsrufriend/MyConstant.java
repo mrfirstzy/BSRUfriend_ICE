55e9bf738174f5a552d7c2d916e60f172b8e09b7
apirat_BSRU <ikool009@hotmail.com>
2017-02-17 14:55:54 +0700

    ListView showImage and name OK

---
+++ app/src/main/java/appewtc/masterung/bsrufriend/MyConstant.java
@@ -0,0 +1,15 @@
+package appewtc.masterung.bsrufriend;
+
+/**
+ * Created by ikool009 on 17/2/2560.
+ */
+
+public class MyConstant {
+
+    // Explicit
+    private String urlPHPString = "http://swiftcodingthai.com/bsru/get_user_master.php";
+
+    public String getUrlPHPString() {
+        return urlPHPString;
+    }
+}//main class
